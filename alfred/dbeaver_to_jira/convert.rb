6ee9c64560f1ba9135b4d68ed5b8e5b12d7bced8
mfham <mfham>
2025-12-08 01:26:33 +0900

    fix: ruby-lsp-rspec-gui-exec-bug

--- alfred/dbeaver_to_jira/convert.rb
+++ alfred/dbeaver_to_jira/convert.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
-clipboard = `pbpaste`
+clipboard = `pbpaste`.force_encoding('UTF-8')
 lines = clipboard.strip.split("\n")
 
 lines.delete_at(1) if lines.size > 1 && lines[1].match?(/^-+\+/)
